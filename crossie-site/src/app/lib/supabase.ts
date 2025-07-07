4e4e0b53818dd5e607bb06ceb23a2477734999eb
hashemalo <hhaasshhemm@gmail.com>
2025-07-07 11:07:31 -0400

    testy

--- crossie-site/src/app/lib/supabase.ts
+++ crossie-site/src/app/lib/supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 
 declare const chrome: any
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = "https://sxargqkknhkcfvhbttrh.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4YXJncWtrbmhrY2Z2aGJ0dHJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3MzEyMDAsImV4cCI6MjA2NjMwNzIwMH0.Q70cLGf69Al2prKMDSkCTnCGTuiKGY-MFK2tQ1g2T-k"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
